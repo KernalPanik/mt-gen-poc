6619933f48f0f992952a40bf19c82900382c1458
KernalPanik <lukas.michnevic@protonmail.com>
2024-06-14 23:09:37 +0300

    add canonical MR and some utils

---
+++ canonicalMR.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+inline int numericAddition(int x, int constant)
+{
+    return x + constant;
+}
+
+inline int numericMultiplication(int x, int constant)
+{
+    return x * constant;
+}
---
+++ utils.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <random>
+
+inline int getRandomInt(int minVal, int maxVal)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distr(minVal, maxVal);
+
+    return distr(gen);
+}
+
+inline void fillRandom(int minVal, int maxVal, int* arr, size_t arrSize)
+{
+    for (size_t i = 0; i < arrSize; i++)
+    {
+        arr[i] = getRandomInt(minVal, maxVal);
+    }
+}
+
+inline double cosine_similarity(long *l1, long *l2, int arrSize)
+{
+    double dot = 0.0, denom_a = 0.0, denom_b = 0.0 ;
+    
+    for (int i = 0; i < arrSize; i++) {
+        dot += l1[i] * l2[i] ;
+        denom_a += l1[i] * l1[i] ;
+        denom_b += l2[i] * l2[i] ;
+    }
+
+    return dot / (sqrt(denom_a) * sqrt(denom_b)) ;
+}
